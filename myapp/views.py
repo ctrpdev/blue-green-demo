5eca47b79ec2868c2a6bfd4e50e412fbe484864d
ctrp <ctrpdev@gmail.com>
2025-10-17 15:34:01 -0300

    add: actualizado

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("¡Bienvenido al proyecto Blue Demo! Actualizado")
+    return HttpResponse("¡Bienvenido al proyecto Blue Demo! Actualizado con éxito.")
